9c7386ae6a8cb7787513b3732afe7e5a5a11a222
MagicSimba <magicsimba@outlook.com>
2026-07-14 13:48:31 +0800

    缩放图片比例

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-set -e
-
-docker run --rm -it -p 8000:8000 -v ./docs:/docs/docs -v ./mkdocs.yml:/docs/mkdocs.yml --name zensical_music zensical/zensical:0.0.43
+docker run --rm -it -p 8000:8000 -v ./docs:/docs/docs -v ./mkdocs.yml:/docs/mkdocs.yml --name zensical_music zensical/zensical:latest
--- sync-github.sh
+++ sync-github.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 rsync -av --delete --inplace --exclude='**.git' --exclude='**.github' $PWD/ /mnt/d/WorkSpace/Github/MusicTheory-Note/
